9f80a82fd755ffc11bd894fb169a451c8e2d6191
unknown <mohamed.hany556@gmail.com>
2021-03-23 23:09:10 +0200

    styling pages

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20 + 10)
         const camp = new Campground({
             location: `${cities[random].city}, ${cities[random].city}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/2184453',
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero ut iure labore voluptatem cum ab architecto eum perferendis repellat in consectetur quam id tenetur molestiae reprehenderit, alias eveniet debitis. Perferendis.",
+            price: price
         })
         await camp.save();
     }
