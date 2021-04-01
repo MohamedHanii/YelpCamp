9a7a23791ac464271b689082e299ab42aa25e4d3
MohamedHanii <mohamed.hany556@gmail.com>
2021-04-02 00:05:35 +0200

    fixing

--- index.js
+++ index.js
@@ -163,6 +163,7 @@ app.use((err, req, res, next) => {
     res.render('error', { err });
 });
 
-app.listen(3000, (req,res) => {
-    console.log("Serving on port 3000")
+const port = process.env.PORT || 3000;
+app.listen(port, (req,res) => {
+    console.log(`Serving on port ${port}`)
 });
